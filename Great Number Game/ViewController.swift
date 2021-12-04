9f0994e275c876ba043b7ba5c4f2eeaf0b8d4c53
Rehab B <79226830+er-otaibi@users.noreply.github.com>
2021-12-05 01:18:50 +0300

    number game

--- Great Number Game/ViewController.swift
+++ Great Number Game/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var numberGuess: UITextField!
+
+    var answer = 44
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func submit(_ sender: UIButton) {
+        
+        if let number = Int(numberGuess.text!) {
+            
+            if number == answer {
+            let alert = UIAlertController(title: "Correct", message: "\(number) was correct !!", preferredStyle: UIAlertController.Style.alert)
+
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "Play again", style: UIAlertAction.Style.default, handler: nil))
+                
+                alert.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.green.withAlphaComponent(0.2)
+
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+                
+            } else if number < answer {
+                let alert = UIAlertController(title: "Worng", message: "\(number) is too low !!", preferredStyle: UIAlertController.Style.alert)
+
+                // add an action (button)
+                alert.addAction(UIAlertAction(title: "Guess again", style: UIAlertAction.Style.default, handler: nil))
+                
+                alert.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+            }else {
+                
+                let alert = UIAlertController(title: "Worng", message: "\(number) is to high !!", preferredStyle: UIAlertController.Style.alert)
+
+                // add an action (button)
+                alert.addAction(UIAlertAction(title: "Guess again", style: UIAlertAction.Style.default, handler: nil))
+                
+                alert.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.red.withAlphaComponent(0.2)
+
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    
+      
 }
 
+}
